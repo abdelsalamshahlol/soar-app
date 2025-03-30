5865de2c20a89e3cbe21fbf329efa99808f37cce
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2025-03-30 13:26:55 +0200

    Input field component created

---
+++ app/components/inputField.tsx
@@ -0,0 +1,36 @@
+import { clsx } from 'clsx';
+import { useId } from 'react';
+
+export interface InputFieldProps {
+  title?: string;
+  placeholder?: string;
+  id?: string;
+  type?: HTMLInputElement['type'];
+  disabled?: boolean;
+  value?: string;
+  required?: boolean;
+}
+
+export function InputField({ title, value, placeholder, id, type = 'text', disabled, required }: InputFieldProps) {
+  const inputId = id || useId();
+
+  return (
+    <label htmlFor={inputId} className="font-inter text-base leading-none font-normal">
+      {title && <span className="block pb-3 text-dark-gray">{title}</span>}
+      <input
+        id={inputId}
+        value={value}
+        disabled={disabled}
+        placeholder={placeholder}
+        type={type}
+        required={required}
+        aria-disabled={disabled}
+        aria-invalid={disabled ? 'true' : 'false'}
+        aria-label={!title ? placeholder : undefined}
+        className={clsx(
+          'disabled:cursor-not-allowed rounded-[15px] border border-input-border-white bg-white h-[50px] w-full placeholder-input-field-placeholder-blue px-5 py-4 focus:outline-2 focus:outline-blue-500'
+        )}
+      />
+    </label>
+  );
+}
